32be8f4863057dd1db1729bd1aa2ce6bf6fe1d2c
evnit <evnithn@gmail.com>
2014-03-31 19:01:08 +0700

    Update and rename full_e120.mk to full_e120k.mk

--- full_e120k.mk
+++ full_e120k.mk
@@ -30,11 +30,11 @@ BOARD_HAVE_NFC := true
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from celoxhd device
-$(call inherit-product, device/samsung/e120/device.mk)
+$(call inherit-product, device/samsung/e120k/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_e120
-PRODUCT_DEVICE := SHV-E120K
+PRODUCT_NAME := full_e120k
+PRODUCT_DEVICE := e120k
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := SHV-E120K
