09fbe9c1c54dbf4bd25b5d65c0936d5f8c4d1edd
Le Anh Dung <evnithn@gmail.com>
2015-02-21 20:28:17 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@
 
 # Assert
 TARGET_BOARD_INFO_FILE ?= device/samsung/e120k/board-info.txt
-TARGET_OTA_ASSERT_DEVICE := SHV-E120K,e120k,dali
+TARGET_OTA_ASSERT_DEVICE := SHV-E120K,e120k
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom kgsl.mmutype=gpummu usb_id_pin_rework=true no_console_suspend=true zcache msm_watchdog.appsbark=0 msm_watchdog.enable=1 loglevel=4 androidboot.selinux=permissive
